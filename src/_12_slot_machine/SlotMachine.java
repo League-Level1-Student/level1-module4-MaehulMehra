f1b749b5e3c662474244de88990ee50d69cff6d2
maehu <maehu@192.168.1.14>
2021-06-03 16:57:27 -0700

    finished

--- src/_12_slot_machine/SlotMachine.java
+++ src/_12_slot_machine/SlotMachine.java
@@ -11,9 +11,13 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class SlotMachine implements ActionListener {
+	Icon cherryIcon;
+	Icon limeIcon;
+	Icon orangeIcon;
 	JFrame frame = new JFrame ();
 	JPanel panel = new JPanel ();
 	JLabel label1 = new JLabel ();
@@ -23,15 +27,50 @@ public class SlotMachine implements ActionListener {
 	Random ran = new Random ();
 	int slot;
 	void makeSpinner () {
-		slot = ran.nextInt(3);
 		frame.setVisible(true);
 		frame.add(panel);
 		panel.add(button);
 		panel.add(label1);
 		panel.add(label2);
 		panel.add(label3);
+		cherryIcon = createLabelImage ("cherry-java.jpg");
+		limeIcon = createLabelImage ("limejava.png");
+		orangeIcon = createLabelImage ("orangejava.jpg");
+		setIcon ();
 		frame.pack();
 		button.addActionListener(this);
+	}
+	void setIcon () {
+		slot = ran.nextInt(3);
+		if (slot == 0) {
+			label1.setIcon(cherryIcon);
+		}
+		else if (slot == 1) {
+			label1.setIcon(limeIcon);
+		}
+		else if (slot == 2) {
+			label1.setIcon(orangeIcon);
+		}
+		slot = ran.nextInt (3);
+		if (slot == 0) {
+			label2.setIcon(cherryIcon);
+		}
+		else if (slot == 1) {
+			label2.setIcon(limeIcon);
+		}
+		else if (slot == 2) {
+			label2.setIcon(orangeIcon);
+		}
+		slot = ran.nextInt (3);
+		if (slot == 0) {
+			label3.setIcon(cherryIcon);
+		}
+		else if (slot == 1) {
+			label3.setIcon(limeIcon);
+		}
+		else if (slot == 2) {
+			label3.setIcon(orangeIcon);
+		}
 	}
 	private Icon createLabelImage(String fileName) {
         URL imageURL = getClass().getResource(fileName);
@@ -44,6 +83,11 @@ public class SlotMachine implements ActionListener {
 }
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
+		if (label1.getIcon () == label2.getIcon () && label1.getIcon() == label3.getIcon()) {
+			JOptionPane.showMessageDialog(null, "YOU WIN!");
+		}
+		setIcon ();
+		frame.pack();
 		// TODO Auto-generated method stub
 		
 	}
--- src/_12_slot_machine/SlotMachineRunner.java
+++ src/_12_slot_machine/SlotMachineRunner.java
@@ -2,6 +2,7 @@ package _12_slot_machine;
 
 public class SlotMachineRunner {
 	public static void main(String[] args) {
-		
+		SlotMachine slot = new SlotMachine ();
+		slot.makeSpinner();
 	}
 }
